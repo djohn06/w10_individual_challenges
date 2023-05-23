e5ab29952769f9df3d653696f5feb01329257680
djohn06 <danielajohn1406@gmail.com>
2023-05-23 11:30:46 +0100

    deposit & bankBalance funcs created. currentBalance() func to pass tests

--- bank_tech_test/bankStatement.js
+++ bank_tech_test/bankStatement.js
@@ -1,10 +1,33 @@
 class BankStatement {
   constructor() {
-    this.account = 0;
+    this.account = [];
   }
 
-  bankBalance() {
-    return this.account;
+  deposit(date, amount) {
+    const balance = this.bankBalance(amount);
+    const transaction = {
+      date: date,
+      credit: amount,
+      debit: "",
+      balance: balance
+    };
+    this.account.unshift(transaction); // unshift enters new object first in the array
+    console.log(this.account);
+  }
+
+  bankBalance(amount) {
+    if (this.account.length > 0) { 
+      const previousBalance = parseInt(this.account[0].balance); // parseInt converts a string to a number
+      return previousBalance + amount;
+    }
+    return amount;
+  }
+
+  currentBalance() {
+    if (this.account.length > 0) {
+      return this.account[0].balance;
+    }
+    return 0
   }
 }
 
